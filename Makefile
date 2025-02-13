d2f768a4af3a8e8c4a94b3ff6cd60674a6a8d404
Artem Tuchinskiy <a.tuchinskiy@ddl.com>
2025-02-13 21:56:19 +0400

    WIP

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-docker-compose-file=./test/docker-compose.yml
+docker-compose-file=./test/docker-compose.yaml
 
 test-full: up
 	@go test ./...
@@ -8,12 +8,13 @@ test:
 	@go test ./...
 
 up:
-	docker-compose -f ${docker-compose-file} up -d --build
+	docker-compose -f ${docker-compose-file} up -d
 
 	sleep 3
 
 	docker-compose -f ${docker-compose-file} exec -T postgres psql -U postgres  < ./test/fixtures/postgres.sql
 	docker-compose -f ${docker-compose-file} exec -T clickhouse clickhouse-client -mn < ./test/fixtures/clickhouse.sql
+	# docker-compose -f ${docker-compose-file} exec -T clickhouse-1 clickhouse-client -mn < ./test/fixtures/clickhouse-cluster.sql
 
 down:
 	docker-compose -f ${docker-compose-file} down --volumes
--- test/fixtures/clickhouse-cluster.sql
+++ test/fixtures/clickhouse-cluster.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS pg;
+CREATE DATABASE IF NOT EXISTS pg ON CLUSTER ch;
 
-CREATE TABLE IF NOT EXISTS pg.pg_stat_statements (
+CREATE TABLE IF NOT EXISTS pg.pg_stat_statements ON CLUSTER ch (
      created_date Date DEFAULT today(),
      created_at UInt32 DEFAULT toUInt32(now()) Codec(Delta, ZSTD),
      created_hour UInt32 DEFAULT toUInt32(toStartOfHour(now())) Codec(Delta, ZSTD),
@@ -24,15 +24,15 @@ CREATE TABLE IF NOT EXISTS pg.pg_stat_statements (
      temp_blks_written Float64,
      blk_read_time Float64,
      blk_write_time Float64
-) ENGINE = MergeTree()
+) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/{shard}/{table}', '{replica}')
     PARTITION BY created_date
     ORDER BY (created_hour, hostname, created_at, datname, username)
     TTL created_date + toIntervalDay(3)
     SETTINGS index_granularity = 8192;
 
-CREATE TABLE IF NOT EXISTS pg.pg_stat_statements_buffer AS pg.pg_stat_statements ENGINE = Buffer(pg, pg_stat_statements, 16, 10, 30, 1000, 10000, 1000000, 10000000);
+CREATE TABLE IF NOT EXISTS pg.pg_stat_statements_buffer ON CLUSTER ch AS pg.pg_stat_statements ENGINE = Buffer(pg, pg_stat_statements, 16, 10, 30, 1000, 10000, 1000000, 10000000);
 
-CREATE TABLE IF NOT EXISTS pg.pg_statio_tables (
+CREATE TABLE IF NOT EXISTS pg.pg_statio_tables ON CLUSTER ch (
    created_date Date DEFAULT today(),
    created_at UInt32 DEFAULT toUInt32(now()) Codec(Delta, ZSTD),
    created_hour UInt32 DEFAULT toUInt32(toStartOfHour(now())) Codec(Delta, ZSTD),
@@ -60,15 +60,15 @@ CREATE TABLE IF NOT EXISTS pg.pg_statio_tables (
    autovacuum_count Float64,
    analyze_count Float64,
    autoanalyze_count Float64
-) ENGINE = MergeTree()
+) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/{shard}/{table}', '{replica}')
  PARTITION BY created_date
  ORDER BY (created_hour, hostname, created_at, datname)
  TTL created_date + toIntervalDay(3)
  SETTINGS index_granularity = 8192;
 
-CREATE TABLE IF NOT EXISTS pg.pg_statio_tables_buffer AS pg.pg_statio_tables ENGINE = Buffer(pg, pg_statio_tables, 16, 10, 30, 1000, 10000, 1000000, 10000000);
+CREATE TABLE IF NOT EXISTS pg.pg_statio_tables_buffer ON CLUSTER ch AS pg.pg_statio_tables ENGINE = Buffer(pg, pg_statio_tables, 16, 10, 30, 1000, 10000, 1000000, 10000000);
 
-CREATE TABLE IF NOT EXISTS pg.pg_table_size (
+CREATE TABLE IF NOT EXISTS pg.pg_table_size ON CLUSTER ch (
    created_date Date DEFAULT today(),
    created_at UInt32 DEFAULT toUInt32(now()) Codec(Delta, ZSTD),
    created_hour UInt32 DEFAULT toUInt32(toStartOfHour(now())) Codec(Delta, ZSTD),
@@ -80,10 +80,10 @@ CREATE TABLE IF NOT EXISTS pg.pg_table_size (
    n_dead_tup Float64,
    size Float64,
    idx_size Float64
-) ENGINE = MergeTree()
+) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/{shard}/{table}', '{replica}')
   PARTITION BY created_date
   ORDER BY (created_hour, hostname, created_at, datname)
   TTL created_date + toIntervalDay(12)
   SETTINGS index_granularity = 8192;
 
-CREATE TABLE IF NOT EXISTS pg.pg_table_size_buffer AS pg.pg_table_size ENGINE = Buffer(pg, pg_table_size, 16, 10, 30, 1000, 10000, 1000000, 10000000);
+CREATE TABLE IF NOT EXISTS pg.pg_table_size_buffer ON CLUSTER ch AS pg.pg_table_size ENGINE = Buffer(pg, pg_table_size, 16, 10, 30, 1000, 10000, 1000000, 10000000);
